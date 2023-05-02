4d93b52a875bcda6fa8e5454b4f23a8063a8b3a6
aiuto250.ja@gmail.com <aiuto250.ja@gmail.com>
2023-05-02 13:23:26 +0900

    20230502

--- apis/user.php
+++ apis/user.php
@@ -11,6 +11,7 @@
 ?>
 
 <?php
+session_start();
 include('./server.php');
 // 2023-04-11 13:33:20 途中
 // echo json_encode($_REQUEST, JSON_UNESCAPED_UNICODE);
@@ -19,25 +20,62 @@ class userapt extends mainacc
 {
     protected $user = "";
     protected $pas = "";
+    // function __construct()
+    // {
+    //     // parent::__construct();
+    // }
 
-    function __construct()
+    // public function usercheck()
+    // {
+    //     echo json_encode($_REQUEST, JSON_UNESCAPED_UNICODE);
+    // }
+
+
+    // 2023-04-25 13:55:24 土中
+    public function userlgin()
     {
+        try {
+            $sql = "select * from user where Ename=:Ename";
+            $sql = "select pass_HASH from user where Ename=:Ename";
+            $this->accsee();
+            $pr = $this->dbh->prepare($sql);
+            $pr->execute(array('Ename' => $_POST['name']));
+            // $pr->execute(array('Ename' => 'tkk'));
+            $temp = $pr->fetchAll();
 
+            if (count($temp) == 0) {
+                echo json_encode(array("loginStatus" => "Enameerror"), JSON_UNESCAPED_UNICODE);
+                return;
+            }
+            if (password_verify($_POST['password'],   $temp[0]['pass_HASH'])) {
+                // echo json_encode($temp[0], JSON_UNESCAPED_UNICODE);
+                echo json_encode(array("loginStatus" => "LoginOK"));
+                return;
+            } else {
+                echo json_encode(array("loginStatus" => "passerror"), JSON_UNESCAPED_UNICODE);
+                return;
+            }
+        } catch (PDOException $e) {
+            $this->printError($e);
+        }
     }
 
-    public function usercheck()
+    public function test()
     {
-        echo json_encode($_REQUEST, JSON_UNESCAPED_UNICODE);
+        $v = '123456';
+        $b = password_hash($v, PASSWORD_DEFAULT);
+        $c = password_hash($v, PASSWORD_DEFAULT);
+        echo $b;
+        echo '<br>';
+        echo $c;
+        echo '<br>';
+        echo password_verify('33123456', $c);
     }
-
-
-
-
 }
 
-// echo json_encode($_REQUEST, JSON_UNESCAPED_UNICODE);
 $ks = new userapt();
-$ks->usercheck();
+$ks->userlgin();
+// $ks->test();
 
 
 
--- js/login.js
+++ js/login.js
@@ -17,7 +17,7 @@ for (temp of document.cookie.split(';')) {
 class useraccsee {
     // Ajax
     async getopt(opt, callback, udate) {
-        console.log("TAG", '2023-04-07 12:51:01')
+        console.log("login.js--", '2023-04-07 12:51:01')
         let url = './apis/user.php?' + opt;
 
         console.log(url);
@@ -34,36 +34,34 @@ class useraccsee {
                 let bb = await b.json();
                 callback(bb);
             } else {
-                console.log("TAG", '400.Error', '2023-04-07 12:58:31');
+                console.log("login.js--", '400.Error', '2023-04-07 12:58:31');
             }
         } catch (error) {
             console.log(error);
-            console.log("TAG", '400 Error', '2023-04-07 12:51:55');
+            console.log("login.js--", '400 Error', '2023-04-07 12:51:55');
         }
     }
 
-    // PHPセッション 一致するかを確認する
-    // ログインの場合
+    // ログイン
     userlogin() {
-        console.log("TAG", "2023-04-10 13:10:05")
-        let lf = new FormData(document.getElementById('login_from'));
-        for (let key of lf.keys()) {
-            if (lf.get(key) === "") {
+        console.log("login.js--", "2023-04-10 13:10:05")
+        let loginfrom = new FormData(document.getElementById('login_from'));
+        for (let key of loginfrom.keys()) {
+            if (loginfrom.get(key) === "") {
                 alert(key + "空");
                 break;
             }
         }
-        this.getopt("opt=login", this.ucallback, lf);
+        this.getopt("opt=login", this.userckcallback, loginfrom);
     }
 
-    // 新規登録の場合
-    ucallback(temp) {
+    userckcallback(temp) {
         console.log(temp);
-        console.log("TAG", '2023-04-10 13:17:28');
+        console.log("login.js--", '2023-04-10 13:17:28');
     }
-
-    // 
 }
+
+
 
 window.addEventListener('load', function () {
     console.log('function test');
@@ -76,81 +74,6 @@ window.addEventListener('load', function () {
 
 
 
-
-
-
-
-// OLD
-
-// セッションチェック
-async function ck(phpsessionid) {
-    let url = 'apis/user.php';
-    let ckd = new FormData();
-    ckd.append('PHPCKID', phpsid);
-    ckd.append('method', 'userlogin');
-    console.log('debug---', phpsessionid);
-    for (v of ckd) {
-        console.log('debug---', v);
-    }
-    try {
-        await fetch(url, {
-            method: 'POST',
-            body: ckd
-        })
-            .then(response => {
-                return response.text();
-            })
-            .then(vvv => {
-                console.log(vvv)
-            })
-    } catch (error) {
-        console.log(error);
-    } finally {
-        console.log('debug--', 'end');
-    }
-}
-
-
-// ユーザーログイン
-function userin() {
-    const but = document.querySelector('button');
-    const fd = document.querySelector('form');
-    but.addEventListener("click", function () {
-        let fdd = new FormData(fd);
-        fdd.append('phpsid', phpsid);
-
-        for (let temp of fdd.entries()) {
-            console.log(temp);
-        }
-
-        fdd.forEach((val, key) => {
-            console.log(key, val)
-        });
-
-        fetch('apis/user.php', {
-            method: 'POST',
-            body: fdd
-        })
-            .then(response => response.text())
-            .then(data => {
-                console.log(data);
-                console.log(document.cookie);
-                // alert("stop");
-                // window.location.href = 'plist.html';
-            });
-    });
-}
-
-
-// window.addEventListener("load", function () {
-//     let xx = document.getElementById("login")
-//     xx.addEventListener("click", function () {
-//         userin();
-//         // ck(phpsid);
-//         console.log("2023-04-06 13:39:12");
-//     })
-
-// });
 
 
 
